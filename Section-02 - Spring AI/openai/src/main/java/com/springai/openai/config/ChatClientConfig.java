5941775a00bf8a50f92887dac74dce93338b9ba8
AVNISH\avnis <avnish.shrivastava@outlook.com>
2026-04-19 14:57:25 +0530

    section-2 advisors commit

--- Section-02 - Spring AI/openai/src/main/java/com/springai/openai/config/ChatClientConfig.java
+++ Section-02 - Spring AI/openai/src/main/java/com/springai/openai/config/ChatClientConfig.java
@@ -1,16 +1,33 @@
 package com.springai.openai.config;
 
+import com.springai.openai.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class ChatClientConfig {
 
+    ChatOptions chatOptions = ChatOptions.builder().model("gpt-4o-mini")
+            .temperature(0.8).build();
+
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
         return chatClientBuilder
-                .defaultSystem("I am an HR assistant, I can only answer related to HR policies")
+                .defaultOptions(chatOptions)
+                .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
+                .defaultSystem("""
+                        You are an internal HR assistant. Your role is to help\s
+                        employees with questions related to HR policies, such as\s
+                        leave policies, working hours, benefits, and code of conduct.
+                        If a user asks for help with anything outside of these topics,\s
+                        kindly inform them that you can only assist with queries related to\s
+                        HR policies.
+                        """)
                 .defaultUser("How can you help me")
                 .build();
     }
--- Section-02 - Spring AI/openai/src/main/java/com/springai/openai/controller/ChatController.java
+++ Section-02 - Spring AI/openai/src/main/java/com/springai/openai/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.springai.openai.controller;
 
+import com.springai.openai.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +14,24 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    public ChatController(ChatClient chatClient) {
+
+        this.chatClient = chatClient;
     }
 
     @GetMapping("/chat")
     public String getResponse(@RequestParam("message") String message) {
-        return chatClient.prompt()
-                .system("I am an HR assistant, I can only answer related to HR policies")
-                .user(message).call().content();
+        return chatClient
+                .prompt()
+                /*.system("""
+                        You are an internal IT helpdesk assistant. Your role is to assist 
+                        employees with IT-related issues such as resetting passwords, 
+                        unlocking accounts, and answering questions related to IT policies.
+                        If a user requests help with anything outside of these 
+                        responsibilities, respond politely and inform them that you are 
+                        only able to assist with IT support tasks within your defined scope.
+                        """)*/
+                .user(message)
+                .call().content();
     }
 }
