94f688b3cc28e4c2c7bb83c67802f0ecec737831
AVNISH\avnis <avnish.shrivastava@outlook.com>
2026-06-07 12:40:58 +0530

    section-3 chat-memory final commit

--- Section-03 - Spring AI Chat Memory/src/main/java/com/springai/openai/config/ChatClientConfig.java
+++ Section-03 - Spring AI Chat Memory/src/main/java/com/springai/openai/config/ChatClientConfig.java
@@ -1,19 +1,40 @@
 package com.springai.openai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class ChatClientConfig {
 
-    ChatOptions chatOptions = ChatOptions.builder().model("gpt-4o-mini")
-            .temperature(0.8).build();
+    ChatOptions.Builder chatOptions = ChatOptions.builder().model("gpt-4o-mini")
+            .temperature(0.8);
+
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder().maxMessages(10).
+                chatMemoryRepository(jdbcChatMemoryRepository).build();
+
+    }
 
     @Bean(name = "chatMemoryClientConfig")
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        Advisor messageChatMemoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+
         return chatClientBuilder
+                .defaultOptions(chatOptions)
+                .defaultAdvisors(List.of(loggerAdvisor, messageChatMemoryAdvisor))
                 .build();
     }
 }
